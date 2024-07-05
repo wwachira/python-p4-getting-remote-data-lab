98e050b2632ad14e51aea8abb8dfd0e1437b7405
lizwachira <lizwanguwachira@gmail.com>
2024-07-05 05:10:37 +0300

    GET response/JSONresponse; 2Test Pass

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,13 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        response = requests.get(self.url)
+        return response.json()
+    
+get_requester = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+print(get_requester.load_json())
+
